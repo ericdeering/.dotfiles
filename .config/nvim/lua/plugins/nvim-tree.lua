94c5bd78f86fa3aaebd898a043aabca9cbc10d3c
dukesmcswagger <edeering4@gmail.com>
2024-08-02 11:22:36 -0400

    remapped nvim-tree info key

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -8,22 +8,25 @@ return {
   config = function()
 
     local function my_on_attach(bufnr)
-      local api = require "nvim-tree.api"
+      local api = require('nvim-tree.api')
 
       local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
       end
 
-      -- default mappings
       api.config.mappings.default_on_attach(bufnr)
+      -- your removals and mappings go here
 
-    --  -- delete mappings
-    --  vim.keymap.del('n', '<c-k>', '', { buffer = bufnr })
+      -- delete mappings
+      --vim.keymap.del('n', '<c-k>', { buffer = bufnr })
 
-    --  -- custom mappings
-    --    -- example: vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-    --    -- exmpale: vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
-    --  vim.keymap.set('n', '<c-i>', api.tree.node.show_info_popup(), opts('Show file/dir information in NvimTree'))
+      -- custom mappings
+        -- example: vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+        -- exmpale: vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+      --vim.keymap.set('n', '<c-i>', api.node.show_info_popup, opts('Info'))
+
+      -- override mappings
+      vim.keymap.set('n', '<C-u>', api.node.show_info_popup, opts('Info'))
     end
 
     require("nvim-tree").setup {
