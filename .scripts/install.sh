a28119b10223c03175ffc36dc53a6e22741001b2
Eric Deering <edeering4@gmail.com>
2024-09-10 19:58:17 -0400

    create loop to create sym links for dotfiles

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,24 +2,30 @@
 
 function TMUX_SOURCE {
   tmux new-session -d -s "temp" ~/.dotfiles/.scripts/source_tmux.sh
+  tmux kill-server
 }
 
-function LinkFile() {
-  file = $1
+function LinkFile {
+  file=$1
+  echo "File: $file"
   if [ ! -L "~/$file" ]; then
     if [ -f "~/$file" ]; then
       mv "~/$file" "~/$file.bak"
-      echo "Your $file has been backed up as $file.bak"
+      echo "Your ~/$file has been backed up as ~/$file.bak"
     fi
-    ln -s "~/.dotfiles/$file" "~/$file"
+    ln -s ~/.dotfiles/"$file" ~/"$file"
   else
-    echo "Symbolic link already found for $file... skipping"
+    echo "Symbolic link already found for ~/$file... skipping"
   fi
 }
 
-find ~/.dotfiles -type f -depth 1 -exec LinkFile {} ';'
+export -f LinkFile
+find ~/.dotfiles -type f -depth 1 | sed 's:.*/::' |
+	while IFS= read -r line; do
+		LinkFile "$line"
+	done
+
 cd ~/.dotfiles/
 TMUX_SOURCE
 ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
-tmux kill-server
 source ~/.bash_profile
