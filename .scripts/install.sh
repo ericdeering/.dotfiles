eaf8c232b59c015333daae33c552b4ad385d6615
Eric Deering <edeering4@gmail.com>
2024-09-10 22:13:55 -0400

    updated install script to map symbolic links in a more compatible way

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
+testing=false
+
 function TMUX_SOURCE {
   tmux new-session -d -s "temp" ~/.dotfiles/.scripts/source_tmux.sh
   tmux kill-server
 }
 
 function LinkFile {
-  file=$1
-  if [ ! -L ~/"$file" ]; then
-    if [ -f ~/"$file" ]; then
-      mv ~/"$file" ~/"$file".bak
-      echo "Your ~/$file has been backed up as ~/$file.bak"
-    fi
-    if [ "$file" = ".gitignore" ]; then
-      continue
-    fi
-    ln -s ~/.dotfiles/"$file" ~/"$file"
-  else
-    echo "Symbolic link already found for ~/$file... skipping"
+  sourcepath=$1                                   # e.g. /Users/<user>/.dotfiles/.config/nvim
+  installpath=$( echo $1 | sed 's:/.dotfiles::' ) # e.g. /Users/<user>/.config/nvim
+  filename=$( echo $1 | sed 's:.*/::' )           # e.g. nvim
+
+  if [ $testing = true ]; then
+
+    echo "Source Path:	$sourcepath"
+    echo "Install Path:	$installpath"
+    echo "Filename: 	$filename"
+
+    [[ -L "$installpath" ]]                        && echo "Symbolic Link: FOUND" || echo "Symbolic Link: NOT FOUND"
+    [[ -f "$installpath" && ! -L "$installpath" ]] && echo "File:          FOUND" || echo "File:          NOT FOUND"
+    [[ -d "$installpath" && ! -L "$installpath" ]] && echo "Directory:     FOUND" || echo "Directory:     NOT FOUND"
+
+    echo
+
+    return
+
   fi
+
+  if [ -L "$installpath" ]; then
+    echo "Symbolic link already found for $installpath... skipping"
+    return
+  fi
+
+  if [ -f "$installpath" ] || [ -d "$installpath" ]; then
+    mv "$installpath" "$installpath".bak
+    echo "Your $installpath has been backed up as $installpath.bak"
+  fi
+
+  if [ "$file" = ".gitignore" ]; then
+    continue
+  fi
+
+  ln -s "$sourcepath" "$installpath"
 }
 
-export -f LinkFile
-find ~/.dotfiles -type f -depth 1 | sed 's:.*/::' |
+find ~/.dotfiles -type f -depth 1 |
 	while IFS= read -r line; do
 		LinkFile "$line"
 	done
+
+find ~/.dotfiles/.config -type d -depth 1 |
+  while IFS= read -r line; do
+    LinkFile "$line"
+  done
+
+if [ $testing == true ]; then
+  exit 1
+fi
 
 cd ~/.dotfiles/
 TMUX_SOURCE
